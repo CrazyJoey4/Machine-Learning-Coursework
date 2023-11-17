f49ea94a24dd35a5cf7e208be60d249adbd44ecd
CrazyJoey4 <crazyjoeyooi@gmail.com>
2023-11-17 22:07:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,96 @@
 import pandas as pd
 import re
 import nltk
-nltk.download('wordnet')
-nltk.download('punkt')
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-
-from gensim.models import Word2Vec
-
-# For Naive Bayes Classifier
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
 
-# For SVM Classifier
-from sklearn.svm import SVC
 
-#For Optimize (Stacking Ensemble)
-from sklearn.ensemble import VotingClassifier
-from sklearn.ensemble import StackingClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
+def load_data(file_path):
+    df = pd.read_csv(file_path, encoding='latin-1')
+    df = df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1)
+    df = df.rename(columns={"v1": "label", "v2": "text"})
+    df['label'] = df['label'].map({'ham': 0, 'spam': 1})
+    return df
 
 
-
-
-# Load the dataset
-df = pd.read_csv('spam.csv', encoding='latin-1')
-
-# Drop unnecessary columns
-df = df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1)
-
-# Rename columns
-df = df.rename(columns={"v1": "label", "v2": "text"})
-
-# Map labels to binary values
-df['label'] = df['label'].map({'ham': 0, 'spam': 1})
-
-# Text preprocessing functions
 def preprocess_text(text):
-    # Remove unnecessary characters and convert to lowercase
     text = re.sub("[^a-zA-Z]", " ", text.lower())
-    # Tokenization
     words = text.split()
-    # Remove stopwords
     words = [word for word in words if word not in stopwords.words("english")]
-    # Lemmatization
     lemmatizer = WordNetLemmatizer()
     words = [lemmatizer.lemmatize(word) for word in words]
-    # Join the words back into a string
     return ' '.join(words)
 
-# Apply preprocessing to the entire dataset
-df['text'] = df['text'].apply(preprocess_text)
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], test_size=0.2, random_state=42)
+def preprocess_data(df):
+    df['text'] = df['text'].apply(preprocess_text)
+    return df
+
+
+def split_data(df, test_size=0.2, random_state=42):
+    X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], test_size=test_size, random_state=random_state)
+    return X_train, X_test, y_train, y_test
+
+
+def train_naive_bayes(X_train_tfidf, y_train, alpha=1.0):
+    nb_classifier = MultinomialNB(alpha=alpha)
+    nb_classifier.fit(X_train_tfidf, y_train)
+    return nb_classifier
+
+
+def train_svm(X_train_tfidf, y_train, kernel='linear', C=1.0):
+    svm_classifier = SVC(kernel=kernel, C=C)
+    svm_classifier.fit(X_train_tfidf, y_train)
+    return svm_classifier
+
+
+def evaluate_classifier(classifier, X_test_tfidf, y_test):
+    predictions = classifier.predict(X_test_tfidf)
+    accuracy = accuracy_score(y_test, predictions)
+    classification_report_str = classification_report(y_test, predictions)
+    return accuracy, classification_report_str
+
+
+def optimize_naive_bayes(X_train_tfidf, y_train):
+    nb_classifier = MultinomialNB()
+    nb_params = {'alpha': [0.1, 0.5, 1.0, 2.0]}
+    nb_grid_search = GridSearchCV(nb_classifier, nb_params, cv=5, scoring='accuracy')
+    nb_grid_search.fit(X_train_tfidf, y_train)
+    best_alpha = nb_grid_search.best_params_['alpha']
+
+    # Fit the optimized model with the best hyperparameters
+    optimized_nb_classifier = MultinomialNB(alpha=best_alpha)
+    optimized_nb_classifier.fit(X_train_tfidf, y_train)
+
+    return optimized_nb_classifier, best_alpha
+
+
+def optimize_svm(X_train_tfidf, y_train):
+    svm_classifier = SVC(kernel='linear')
+    svm_params = {'C': [0.1, 1, 10]}
+    svm_grid_search = GridSearchCV(svm_classifier, svm_params, cv=5, scoring='accuracy')
+    svm_grid_search.fit(X_train_tfidf, y_train)
+    best_C = svm_grid_search.best_params_['C']
+
+    # Fit the optimized model with the best hyperparameters
+    optimized_svm_classifier = SVC(kernel='linear', C=best_C)
+    optimized_svm_classifier.fit(X_train_tfidf, y_train)
+
+    return optimized_svm_classifier, best_C
+
+
+# Load and preprocess data
+df = load_data('spam.csv')
+df = preprocess_data(df)
+
+# Split the dataset
+X_train, X_test, y_train, y_test = split_data(df)
 
 # Convert text data to numerical features using CountVectorizer
 count_vectorizer = CountVectorizer()
@@ -73,37 +103,12 @@ X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 X_test_tfidf = tfidf_transformer.transform(X_test_counts)
 
 
-# Tokenize the text into words
-tokenized_text = df['text'].apply(word_tokenize)
 
-# Train Word2Vec model
-word2vec_model = Word2Vec(sentences=tokenized_text, vector_size=100, window=5, min_count=1, workers=4)
+# Train Naive Bayes Classifier
+naive_bayes_classifier = train_naive_bayes(X_train_tfidf, y_train)
 
-# Function to get the vector representation of a sentence
-def get_vector(sentence, model):
-    vector = [model.wv[word] for word in sentence if word in model.wv]
-    return sum(vector) / len(vector) if vector else [0] * model.vector_size
-
-# Apply Word2Vec to the entire dataset
-df['text_vector'] = tokenized_text.apply(lambda x: get_vector(x, word2vec_model))
-
-# Split the dataset into training and testing sets
-X_train_w2v, X_test_w2v, y_train_w2v, y_test_w2v = train_test_split(df['text_vector'], df['label'], test_size=0.2, random_state=42)
-
-
-# First algorithm : Naive Bayes Classifier
-# Create Naive Bayes classifier
-naive_bayes_classifier = MultinomialNB()
-
-# Train the classifier on the TF-IDF transformed data
-naive_bayes_classifier.fit(X_train_tfidf, y_train)
-
-# Predictions on the test set
-predictions_nb = naive_bayes_classifier.predict(X_test_tfidf)
-
-# Evaluate the performance
-accuracy_nb = accuracy_score(y_test, predictions_nb)
-classification_report_nb = classification_report(y_test, predictions_nb)
+# Evaluate Naive Bayes Classifier
+accuracy_nb, classification_report_nb = evaluate_classifier(naive_bayes_classifier, X_test_tfidf, y_test)
 
 print("Naive Bayes Classifier:")
 print("Accuracy:", accuracy_nb)
@@ -111,80 +116,135 @@ print("Classification Report:\n", classification_report_nb)
 
 
 
-# Second algorithm : Support Vector Machines (SVM) Classifier
-# Create SVM classifier
-svm_classifier = SVC(kernel='linear')  # You can experiment with different kernels (linear, rbf, etc.)
+# Optimize Naive Bayes Classifier
+optimized_nb_classifier, best_alpha_nb = optimize_naive_bayes(X_train_tfidf, y_train)
 
-# Train the classifier on the TF-IDF transformed data
-svm_classifier.fit(X_train_tfidf, y_train)
+# Evaluate Optimized Naive Bayes Classifier
+accuracy_optimized_nb, classification_report_optimized_nb = evaluate_classifier(optimized_nb_classifier, X_test_tfidf, y_test)
 
-# Predictions on the test set
-predictions_svm = svm_classifier.predict(X_test_tfidf)
+print("\nOptimized Naive Bayes Classifier with alpha =", best_alpha_nb)
+print("Accuracy:", accuracy_optimized_nb)
+print("Classification Report:\n", classification_report_optimized_nb)
 
-# Evaluate the performance
-accuracy_svm = accuracy_score(y_test, predictions_svm)
-classification_report_svm = classification_report(y_test, predictions_svm)
+
+
+# Train SVM Classifier
+svm_classifier = train_svm(X_train_tfidf, y_train)
+
+# Evaluate SVM Classifier
+accuracy_svm, classification_report_svm = evaluate_classifier(svm_classifier, X_test_tfidf, y_test)
 
 print("\nSupport Vector Machines (SVM) Classifier:")
 print("Accuracy:", accuracy_svm)
 print("Classification Report:\n", classification_report_svm)
 
 
-# To optimize the models : Stacking Ensemble
-# Define base classifiers
-base_classifiers = [
-    ('decision_tree', DecisionTreeClassifier()),
-    ('random_forest', RandomForestClassifier()),
-    ('naive_bayes', MultinomialNB()),
-    ('svm', SVC(kernel='linear'))
-]
 
-# Create the stacking classifier
-stacking_classifier = StackingClassifier(estimators=base_classifiers, final_estimator=LogisticRegression())
+# Optimize SVM Classifier
+optimized_svm_classifier, best_C_svm = optimize_svm(X_train_tfidf, y_train)
 
-# Train the stacking classifier on the TF-IDF transformed data
-stacking_classifier.fit(X_train_tfidf, y_train)
+# Evaluate Optimized SVM Classifier
+accuracy_optimized_svm, classification_report_optimized_svm = evaluate_classifier(optimized_svm_classifier, X_test_tfidf, y_test)
 
-# Predictions on the test set
-predictions_stacking = stacking_classifier.predict(X_test_tfidf)
-
-# Evaluate the performance of the stacking classifier
-accuracy_stacking = accuracy_score(y_test, predictions_stacking)
-classification_report_stacking = classification_report(y_test, predictions_stacking)
-
-print("\nStacking Ensemble Classifier:")
-print("Accuracy:", accuracy_stacking)
-print("Classification Report:\n", classification_report_stacking)
+print("\nOptimized SVM Classifier with C =", best_C_svm)
+print("Accuracy:", accuracy_optimized_svm)
+print("Classification Report:\n", classification_report_optimized_svm)
 
 
 
-# Train Word2Vec model on the entire dataset
-word2vec_model = Word2Vec(sentences=tokenized_text, vector_size=100, window=5, min_count=1, workers=4)
+# For testing
+def preprocess_input(text):
+    # Your preprocessing logic here (similar to what you did with the training data)
+    text = re.sub("[^a-zA-Z]", " ", text.lower())
+    words = text.split()
+    words = [word for word in words if word not in stopwords.words("english")]
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
+    return ' '.join(words)
 
-# Function to get the vector representation of a sentence
-def get_vector(sentence, model):
-    vector = [model.wv[word] for word in sentence if word in model.wv]
-    return sum(vector) / len(vector) if vector else [0] * model.vector_size
 
-# Apply Word2Vec to the entire dataset
-df['text_vector'] = tokenized_text.apply(lambda x: get_vector(x, word2vec_model))
+def classify_input(input_text, classifier):
+    # Preprocess the entire input text
+    preprocessed_input = preprocess_input(input_text)
 
-# Split the dataset into training and testing sets for Word2Vec
-X_train_w2v, X_test_w2v, y_train_w2v, y_test_w2v = train_test_split(df['text_vector'], df['label'], test_size=0.2, random_state=42)
+    # Convert the preprocessed text to numerical features
+    input_counts = count_vectorizer.transform([preprocessed_input])
+    input_tfidf = tfidf_transformer.transform(input_counts)
 
-# Train a classifier (e.g., Logistic Regression) on the Word2Vec transformed data
-from sklearn.linear_model import LogisticRegression
+    # Make predictions using the classifier
+    prediction = classifier.predict(input_tfidf)
 
-logistic_regression_w2v = LogisticRegression()
-logistic_regression_w2v.fit(X_train_w2v.tolist(), y_train_w2v)
+    return prediction[0]
 
-# Predictions on the Word2Vec test set
-predictions_w2v = logistic_regression_w2v.predict(X_test_w2v.tolist())
+def classify_input_optimized(input_text, classifier, count_vectorizer, tfidf_transformer):
+    # Preprocess the entire input text
+    preprocessed_input = preprocess_input(input_text)
 
-# Evaluate the performance of Word2Vec
-accuracy_w2v = accuracy_score(y_test_w2v, predictions_w2v)
-classification_report_w2v = classification_report(y_test_w2v, predictions_w2v)
+    # Convert the preprocessed text to numerical features
+    input_counts = count_vectorizer.transform([preprocessed_input])
+    input_tfidf = tfidf_transformer.transform(input_counts)
 
-print("\nWord2Vec Classifier:")
-print("Accuracy:", accuracy_w2v)
-print("Classification Report:\n", classification_report_w2v)
+    # Make predictions using the optimized classifier
+    prediction = classifier.predict(input_tfidf)
+
+    return prediction[0]
+
+
+# Example usage with a loop
+while True:
+    user_input = input("| Naive Bayes Classifier |\nEnter a sentence to classify (enter 'x' to exit): ")
+
+    if user_input.lower() == 'x':
+        break
+
+    result = classify_input(user_input, naive_bayes_classifier)
+
+    if result == 0:
+        print("Predicted: Ham")
+    else:
+        print("Predicted: Spam")
+
+
+# 2nd Example usage with a loop
+while True:
+    user_input = input("\n\n| Naive Bayes Classifier Optimized |\nEnter a sentence to classify (enter 'x' to exit): ")
+
+    if user_input.lower() == 'x':
+        break
+
+    result_optimized = classify_input_optimized(user_input, optimized_nb_classifier, count_vectorizer, tfidf_transformer)
+
+    if result_optimized == 0:
+        print("Predicted: Ham")
+    else:
+        print("Predicted: Spam")
+
+
+# 3rd Example usage with a loop
+while True:
+    user_input = input("\n\n| SVM Classifier |\nEnter a sentence to classify (enter 'x' to exit): ")
+
+    if user_input.lower() == 'x':
+        break
+
+    result = classify_input(user_input, svm_classifier)
+
+    if result == 0:
+        print("Predicted: Ham")
+    else:
+        print("Predicted: Spam")
+
+
+# 4th Example usage with a loop
+while True:
+    user_input = input("\n\n| SVM Classifier Optimized |\nEnter a sentence to classify (enter 'x' to exit): ")
+
+    if user_input.lower() == 'x':
+        break
+
+    result_optimized = classify_input_optimized(user_input, optimized_svm_classifier, count_vectorizer, tfidf_transformer)
+
+    if result_optimized == 0:
+        print("Predicted: Ham")
+    else:
+        print("Predicted: Spam")
